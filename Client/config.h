a295c8d58db45f4e4bc7ace9a2022e8d56ed5898
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2018-05-07 04:30:05 +0700

    Update: Update client config file

--- Client/config.h
+++ Client/config.h
@@ -3,6 +3,8 @@
 
 /******************** DEFINE ********************/
 #define DEFAULT_PORT_NUMBER 5000
-#define MAX_BUFFER_SIZE     255
+#define MAX_MESSAGE_BUFFER_SIZE     255
+#define TIME_STRING_FORMAT "%Y-%m-%d %H:%M:%S"
+#define MAX_SIZE_OF_USER_LIST 255
 
 #endif /* __CONFIG__ */
